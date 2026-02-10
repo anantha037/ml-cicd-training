e65c4536083d1b3f0ac1c24657e6f8129b9d9a04
anantha037 <ananthan7703@gmail.com>
2026-02-11 02:02:54 +0530

    Versioning

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
+import logging
+from prometheus_client import Counter, Histogram, generate_latest
+from fastapi.responses import Response
+import time
+import os
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+REQUEST_COUNT = Counter(
+    "prediction_requests_total",
+    "Total number of prediction requests"
+)
+
+REQUEST_FAILURES = Counter(
+    "prediction_failure_total",
+    "Total number of failed prediction requests"
+)
+
+REQUEST_LATENCY = Histogram(
+    "prediction_latency_seconds",
+    "Time spent processing prediction"
+)
 
 app = FastAPI()
 
-model = joblib.load('model.joblib')
+ACTIVE_MODEL_VERSION = os.getenv("MODEL_VERSION","v1")
+MODEL_PATH = f"models/model_{ACTIVE_MODEL_VERSION}.joblib"
+logger.info(f"Loading model version: {ACTIVE_MODEL_VERSION}")
+model = joblib.load(MODEL_PATH)
+logger.info("Model loaded successfully")
 
 @app.get("/")
 def home():
@@ -14,11 +44,34 @@ def home():
 
 @app.post("/predict")
 def predict(age:int, salary:int):
-    input_data = np.array([[age, salary]])
-    prediction = model.predict(input_data)
+    start_time = time.time()
+    REQUEST_COUNT.inc()
 
-    return {
-        "age":age,
-        "salary":salary,
-        "prediction":int(prediction[0])
-    }
+
+    try:
+        logger.info(f"Received prediction request: age={age}, salary={salary}")
+
+
+        input_data = np.array([[age, salary]])
+        prediction = model.predict(input_data)
+
+        result = int(prediction[0])
+        logger.info(f"Prediction result: {result}")
+        return {
+            "age":age,
+            "salary":salary,
+            "prediction":result
+        }
+
+    except Exception as e:
+        REQUEST_FAILURES.inc()
+        logger.error(f"Prediction failed: {str(e)}")
+        return {"error":"Prediction failed"}
+
+    finally:
+        REQUEST_LATENCY.observe(time.time() - start_time)
+
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
--- train.py
+++ train.py
@@ -2,6 +2,16 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import joblib
+import os
+import sys
+
+#----- Version handling ----
+MODEL_VERSION = sys.argv[1] if len(sys.argv) > 1 else 'v1'
+
+MODEL_DIR = "models"
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+MODEL_PATH = f"{MODEL_DIR}/model_{MODEL_VERSION}.joblib"
 
 data = pd.read_csv('data/data.csv')
 
@@ -14,6 +24,6 @@ model = LogisticRegression()
 
 model.fit(X_train, y_train)
 
-joblib.dump(model, 'model.joblib')
+joblib.dump(model, MODEL_PATH)
 
-print("Model training completed successfully")
+print(f"Model version {MODEL_VERSION} trained and saved at {MODEL_PATH}")
